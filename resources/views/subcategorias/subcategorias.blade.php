52e976d2bd134a181400075faefdc881d8cf1626
Dinacard <93109341+Dinacard@users.noreply.github.com>
2021-11-03 23:29:25 -0500

    Create subcategorias.blade.php
    Se crea la vista de subcategorias

---
+++ resources/views/subcategorias/subcategorias.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Aqui es el codigo de mis subcategorias.</p>
+
+    <h1>Bienvenido a la seccion de Subcategorias</h1>
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop

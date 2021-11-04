4aab23c7c5a67035817fa6814d928db2cd9970f9
Dinacard <93109341+Dinacard@users.noreply.github.com>
2021-11-03 23:20:39 -0500

    Create pedidos.blade.php
    se crea la vista en la carpeta pedidos

---
+++ resources/views/pedidos.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Aqui es el codigo de mis pedidos.</p>
+
+    <h1>Bienvenido a la seccion de Pedidos</h1>
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+
+@stop

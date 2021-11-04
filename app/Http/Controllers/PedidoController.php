67c6bdc079715d77b6bd259c0453a1d1530f56a5
Dinacard <93109341+Dinacard@users.noreply.github.com>
2021-11-03 23:27:17 -0500

    Create PedidoController.php
    se crea el controller de pedido

---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pedido;
+use Illuminate\Http\Request;
+
+class PedidoController extends controller
+{
+    public function pedidos()
+    {
+        return view('pedidos');
+    }
+}

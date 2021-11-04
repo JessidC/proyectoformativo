09bfce71800d5b32a31c1127644c55adf1ab3be8
JessidC <jessidc95@gmail.com>
2021-11-04 15:29:11 -0500

    Update categorias.blade.php
    recursos

--- resources/views/categorias/categorias.blade.php
+++ resources/views/categorias/categorias.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('content')
-    <p>Aqui es el codigo de mis categorias.</p>
+    <p>Aqui es el codigo de mis categorias. rec </p>
 
     <h1>Bienvenido a la seccion de Categorias</h1>
 @stop
